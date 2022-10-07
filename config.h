f47c0aa7f4d7d04fda11cdea3d716cf480a14efd
Varad Sharma <varad.sharma2009@gmail.com>
2022-10-07 13:03:32 +0530

    update1

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[]  = { "brave", NULL };
 static const char *rofi_run[] = { "rofi", "-show", "drun", NULL };
 static const char *lock[]     = { "slock", NULL};
